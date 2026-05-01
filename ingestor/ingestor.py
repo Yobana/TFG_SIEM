bfcfb93a43ec2a276e9bbba848d599a4e8406373
Yobana <yna1001@alu.ubu.es>
2026-05-02 00:34:04 +0200

    Ingestor actualizado con nueva estructura de eventos

--- ingestor/ingestor.py
+++ ingestor/ingestor.py
@@ -1,19 +1,33 @@
 from pathlib import Path
-from datetime import datetime
 
-
-def parse_line(line, source="test.log"):
+def parse_line(line, source="polvorin.log"):
     """
     Convierte una línea de log en un evento básico.
+    
+     Formato nuevo:
+    timestamp | source | event_type | severity | user_id | access_point | 
+    deposit_id | device_id | result | message
     """
-    return {
-        "timestamp": datetime.now().isoformat(),
-        "source": source,
-        "event_type": "log",
-        "severity": "info",
-        "message": line.strip()
-    }
 
+    parts = [p.strip() for p in line.strip().split("|")]
+
+    if len(parts) != 10:
+        print(f"Línea ignorada: {line.strip()}")
+        return None
+    
+    timestamp, source, event_type, severity, user_id, access_point, deposit_id, device_id, result, message = parts
+    return {
+        "timestamp": timestamp,
+        "source": source,
+        "event_type": event_type,
+        "severity": severity,
+        "user_id": user_id,
+        "access_point": access_point,
+        "deposit_id": deposit_id,
+        "device_id": device_id,
+        "result": result,
+        "message": message
+    }
 
 def read_log_file(file_path):
     """
@@ -29,13 +43,15 @@ def read_log_file(file_path):
     with open(path, "r", encoding="utf-8") as f:
         for line in f:
             if line.strip():
-                events.append(parse_line(line, path.name))
+                event = parse_line(line, path.name)
+                if event:
+                    events.append(parse_line(line, path.name))
 
     return events
 
 
 if __name__ == "__main__":
-    log_file = "logs/test.log"
+    log_file = "logs/polvorin.log"
     events = read_log_file(log_file)
 
     for event in events:
